5996e7c7b6f3ae92968ed77bd4f5c8dd4f6b158c
Gourav Vishwkarma <gouravvish.itscient@gmail.com>
2023-01-31 17:16:51 +0530

    Table UI Change in Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,34 +6,70 @@ Laravel-Admin Dashboard
 
 
 @section('content')
-<div class="card">
-  <div class="card-header">
-    <h5 class="card-category">All Persons List</h5>
-      <h4 class="card-title" id="selector"> Employees Stats</h4>
-  </div>
-  <div class="card-body">
-    <div class="table-responsive">
-      <table class="table">
-        <thead class=" text-primary">
-          <th>Id</th>
-          <th>Name</th>
-          <th>E-mail</th>
-          <th>Salary</th>
-        </thead>
-        <tbody>
-        @foreach ($users as $user)
-          <tr>
-            <td>{{$user->slno}}</td>
-            <td>{{$user->name}}</td>
-            <td>{{$user->email}}</td>
-            <td>$36</td>
-          </tr>
-        @endforeach
-        </tbody>
-      </table>
+<style>
+  table td,
+  table th {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  
+  thead th {
+    color: #fff;
+  }
+  
+  .card {
+    border-radius: .5rem;
+  }
+  
+  .table-scroll {
+    border-radius: .5rem !important;
+  }
+  
+  .table-scroll table thead th {
+    font-size: 1.25rem !important;
+  }
+  thead {
+    top: 0;
+    position: sticky;
+  }
+  </style>
+<div>
+    <div class="card">
+      <div class="card-body p-0">
+        <div class="table-responsive table-scroll" data-mdb-perfect-scrollbar="true" style="position: relative; height: 700px">
+          <table class="table table-striped mb-0">
+            <thead style="background-color: #002d72;">
+              <tr>
+                <th scope="col">Sl.no</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Price</th>
+                <!-- <th scope="col">Spots</th> -->
+              </tr>
+            </thead>
+            <tbody>
+              @if(count($users)>0)
+              @foreach ($users as $user)
+                <tr>
+                  <td>{{$user->slno}}</td>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  <td>$36</td>
+                </tr>
+              @endforeach
+              @else
+              <tr>
+                <td colspan="9" class="text-center" style="font-weight:bolder;">No Found !</td>
+              </tr>
+              @endif
+              </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  </div>
 </div>
+
 @endsection 
 
 @section('scripts')
